7dea97ed95bb6d3521aaefe70ab816f2435441f1
mohini patange <patangemohini5@gmail.com>
2022-05-23 13:55:04 +0530

    Added : Welcome message

---
+++ src/com/bl/basicprograms/BasicCorePrograms.java
@@ -0,0 +1,7 @@
+package com.bl.basicprograms;
+
+public class BasicCorePrograms {
+    public static void main(String[] args) {
+        System.out.println("Welcome to java basic programs!!!");
+    }
+}
